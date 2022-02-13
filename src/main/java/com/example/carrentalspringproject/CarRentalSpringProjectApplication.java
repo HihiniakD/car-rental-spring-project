3452e784ca6126e29f94fb40a2f45ebd7606decd
Dmytro Hihiniak <skrilldima@gmail.com>
2022-02-13 21:11:06 +0200

    Initial commit

---
+++ src/main/java/com/example/carrentalspringproject/CarRentalSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentalspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carrentalspringproject/CarRentalSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrentalspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
